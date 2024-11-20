2020fd176ed10ee05c67ad2a212a485045ec3c74
Nick Zimmermann <59408512+akarcode@users.noreply.github.com>
2024-11-20 23:18:53 +0700

    Add files via upload

---
+++ adobe_metadata.ps1
@@ -0,0 +1,22 @@
+﻿
+
+#$AdobeFile = $PSScriptRoot + '\Testfiles\xmp_photoshop.xml'
+$AdobeFile = $PSScriptRoot + '\Testfiles\xmp_cases.xml'
+$ExifTool = 'C:\Program Files\exiftool\exiftool.exe'
+
+
+Import-Module -Name ($PSScriptRoot + '\ModulesXMP\ModulesXMP.psd1') -Force -DisableNameChecking
+
+
+$AdobeMetadata = Get-AdobeMetadata -AdobeFile $AdobeFile -ExifTool $ExifTool
+
+List-AdobeMetadata $AdobeMetadata
+
+
+<#
+$AdobeMetaData['Basic']['CreatorTool']
+$AdobeMetaData['Basic']['DateandTimeZone']
+$AdobeMetaData['IPTCExtension']['EditedPropertyIPTC']
+$AdobeMetaData['MediaManagement']['DocumentID']
+#>
+
